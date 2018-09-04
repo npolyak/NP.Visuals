de473bb29259fca0d23c4cdd5689da0442a5caf6
npolyak <nick.polyak@verizon.net>
2018-09-03 21:43:33 -0400

    increased the version

--- NP.Visuals/Properties/AssemblyInfo.cs
+++ NP.Visuals/Properties/AssemblyInfo.cs
@@ -9,9 +9,9 @@ using System.Windows.Markup;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NP.Visuals")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("visual utilities and behaviors")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nick Polyak")]
 [assembly: AssemblyProduct("NP.Visuals")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -52,7 +52,7 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: XmlnsDefinition("http://np.com/visuals", "NP.Visuals")]
