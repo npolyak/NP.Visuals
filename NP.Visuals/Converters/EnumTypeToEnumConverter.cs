8f2fc9b2c3a471651ad51ce0b4b8430e3fce679a
npolyak <nick.polyak@verizon.net>
2019-05-19 15:23:31 -0400

    added some missing files.

---
+++ NP.Visuals/Converters/EnumTypeToEnumConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NP.Visuals.Converters
+{
+    public class EnumTypeToEnumConverter : IValueConverter
+    {
+        public static EnumTypeToEnumConverter Instance { get; } = new EnumTypeToEnumConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Type type)
+            {
+                if (!type.IsEnum)
+                    return null;
+
+                return Enum.GetValues(type);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NP.Visuals/Converters/PropEditorDataTemplateSelector.cs
@@ -0,0 +1,39 @@
+﻿using NP.Visuals.Utils;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace NP.Visuals.Converters
+{
+    // receives and object of type DependencyPropertyInfo
+    public class PropEditorDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate StandardTemplate { get; set; }
+
+        public DataTemplate BoolTemplate { get; set; }
+
+        public DataTemplate EnumTemplate { get; set; }
+
+        public DataTemplate BrushTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is DependencyPropertyInfo dpInfo)
+            {
+                Type propType = dpInfo.PropertyType;
+
+                if (propType == typeof(bool))
+                    return BoolTemplate;
+
+                if (propType.IsEnum)
+                    return EnumTemplate;
+
+                if (typeof(Brush).IsAssignableFrom(propType))
+                    return BrushTemplate;
+            }
+
+            return StandardTemplate;
+        }
+    }
+}
