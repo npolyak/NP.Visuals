87ffcfefd740fc534c4797681d680de3cee71ecc
npolyak <nick.polyak@verizon.net>
2019-11-17 16:22:01 -0500

    refactoring related to folder functionality.

---
+++ NP.Visuals/Converters/ObjToTypeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NP.Visuals.Converters
+{
+    public class ObjToTypeConverter : IValueConverter
+    {
+        public static ObjToTypeConverter Instance { get; } = new ObjToTypeConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.GetType();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
