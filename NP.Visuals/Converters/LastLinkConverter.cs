0a029cb68f9da51e794aef00b66a9da442757ab3
npolyak <nick.polyak@verizon.net>
2020-01-24 12:22:54 -0500

    added LastLinkConverter

---
+++ NP.Visuals/Converters/LastLinkConverter.cs
@@ -0,0 +1,34 @@
+﻿using NP.Utilities;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NP.Visuals.Converters
+{
+    public class LastLinkConverter : IValueConverter
+    {
+        public static LastLinkConverter LastPeriodLinkConverter { get; } =
+            new LastLinkConverter(".");
+
+        public string Separator { get; }
+        public LastLinkConverter(string separator)
+        {
+            Separator = separator;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string str = value?.ToString();
+
+            if (str == null)
+                return str;
+
+            return str.SubstrFromTo(Separator, null, false);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
